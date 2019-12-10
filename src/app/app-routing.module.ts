3a3d49a951572a7f17fe0d95698d107e9325de62
charles-augustin <charlesjebalitherson@gmail.com>
2019-12-10 01:01:55 -0500

    Add vehicle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AddVehicleComponent} from './add-vehicle/add-vehicle.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'add-vehicle', component: AddVehicleComponent},
+  { path: '',   redirectTo: '/', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
