8673becd3ee289cd56266ec8feab663b12d5034c
charles-augustin <charlesjebalitherson@gmail.com>
2019-12-11 22:39:51 -0500

    Added datatable with sort, filter, pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'vehicleAngular';
+  opened = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddVehicleComponent } from './add-vehicle/add-vehicle.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, 
+        MatSidenavModule, 
+        MatIconModule, 
+        MatToolbarModule,
+        MatMenuModule,
+        MatTableModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSortModule,
+        MatPaginatorModule} from '@angular/material';
+import {FormsModule} from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -12,7 +24,19 @@ import { AddVehicleComponent } from './add-vehicle/add-vehicle.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSidenavModule,
+    FormsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSortModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
